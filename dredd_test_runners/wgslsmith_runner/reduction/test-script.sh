ede8e1960aa41d13815a4c98ffdd0e4b9b0ae71f
agg22 <amber.gorzynski22@imperial.ac.uk>
2024-04-09 13:43:48 +0100

    Add reducer for wgslsmith test cases

---
+++ dredd_test_runners/wgslsmith_runner/reduction/test-script.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# This file returns 0 if the reduced program is 'interesting'
+
+# Recondition the reduced program
+
+# Extract inputs
+
+# Execute the reconditioned program with inputs
+
+# Check that ouptuts still differ from original outputs (stored somewhere)
+
+# If outputs are valid but differ (i.e. no crashes!), return 0
+
+# Otherwise (same output or crash), return 1
